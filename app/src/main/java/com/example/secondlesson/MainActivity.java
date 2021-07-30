82509e49023a3d9c0a934d18daa04a874ddae444
SergeYudkin <87810884+SergeYudkin@users.noreply.github.com>
2021-07-31 00:49:25 +0300

    Начал дз по обработке нажатий в этот раз успешно

--- app/src/main/java/com/example/secondlesson/MainActivity.java
+++ app/src/main/java/com/example/secondlesson/MainActivity.java
@@ -9,17 +9,29 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private int counter1 = 0;
+    private int value1 =1;
+    private int value2 =2;
+    private int value3 =3;
+    private int value4 =4;
+    private int value5 =5;
+    private int value6 =6;
+    private int value7 =7;
+    private int value8 =8;
+    private int value9 =9;
+    private int value0 =0;
 
-    TextView textView;
+    TextView textView1;
 
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
     Button button0;
-
-
-
-
-
-
 
 
 
@@ -27,23 +39,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.constreint_layout);
 
-        textView = findViewById(R.id.processingButton0);
+        textView1 = findViewById(R.id.textView1);
 
-        textView.setText(String.valueOf(counter1));
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button3 = findViewById(R.id.button3);
+        button3 = findViewById(R.id.button3);
+        button3 = findViewById(R.id.button3);
+        button3 = findViewById(R.id.button3);
+        button3 = findViewById(R.id.button3);
+        button3 = findViewById(R.id.button3);
+        button3 = findViewById(R.id.button3);
 
-        button0.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
+
+
+
+
     }
+
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.button1:
-                counter1++;
-                textView.setText(String.valueOf(counter1));
+                textView1.setText(String.valueOf(value1));
+                break;
+            case R.id.button2:
+                textView1.setText(String.valueOf(value2));
+                break;
+            case R.id.button3:
+                textView1.setText(String.valueOf(value3));
                 break;
         }
-
     }
 }
