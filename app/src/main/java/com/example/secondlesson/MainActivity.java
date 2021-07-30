0fa3f1b4178fa81036e9fc8544e2676b5c5aa8c4
SergeYudkin <87810884+SergeYudkin@users.noreply.github.com>
2021-07-30 19:27:02 +0300

    Начал дз по обработке нажатий

--- app/src/main/java/com/example/secondlesson/MainActivity.java
+++ app/src/main/java/com/example/secondlesson/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.secondlesson;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private int counter1 = 0;
+
+    TextView textView;
+
+    Button button0;
+
+
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.processingButton0);
+
+        textView.setText(String.valueOf(counter1));
+
+        button0.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button1:
+                counter1++;
+                textView.setText(String.valueOf(counter1));
+                break;
+        }
+
     }
 }
