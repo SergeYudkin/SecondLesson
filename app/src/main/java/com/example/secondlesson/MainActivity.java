b58e726922ab5e3ad375e386598c1cf173df03f3
SergeYudkin <87810884+SergeYudkin@users.noreply.github.com>
2021-08-01 23:07:04 +0300

    ДЗ 4 Переделал все кнопки в материал.

--- app/src/main/java/com/example/secondlesson/MainActivity.java
+++ app/src/main/java/com/example/secondlesson/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.secondlesson;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
