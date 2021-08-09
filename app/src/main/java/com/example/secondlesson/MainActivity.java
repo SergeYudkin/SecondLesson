ea5cfe2735a65d9f6cf1be1b7e178be748c50a3f
SergeYudkin <87810884+SergeYudkin@users.noreply.github.com>
2021-08-09 23:28:30 +0300

    Всё сломал

--- app/src/main/java/com/example/secondlesson/MainActivity.java
+++ app/src/main/java/com/example/secondlesson/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.secondlesson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +11,9 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public abstract class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
 
 
     static String TAG = "values";
@@ -39,20 +42,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     Values values;
 
-    static final int light = 1;
-    static final int dark = 2;
 
-
-    static final String KEY_ST = "st";
-    static final String KEY_CURRENT_ST = "current_st";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(getRaalStyle(getCurrentStyle()));
         setContentView(R.layout.constreint_layout);
 
-        init();
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent);
+
+
 
         initView();
         initListeners();
@@ -61,18 +61,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private void init() {
-        ((RadioButton)findViewById(R.id.dark)).setOnClickListener(this);
-        ((RadioButton)findViewById(R.id.light)).setOnClickListener(this);
-        switch (getCurrentStyle()){
-            case 1:
-                ((RadioButton)findViewById(R.id.dark)).setChecked(true);
-                break;
-            case 2:
-                ((RadioButton)findViewById(R.id.light)).setChecked(true);
-                break;
-        }
-    }
+
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -152,43 +141,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.buttonMinus:
                 textView1.setText(String.valueOf(values.getValueMinus()));
                 break;
-            case R.id.dark:
-                setCurrentStyle(dark);
-                break;
-            case R.id.light:
-                setCurrentStyle(light);
-                break;
 
             default:
                 break;
 
         }
-        recreate();
 
-    }
-
-    private void setCurrentStyle(int currentStyle) {
-        SharedPreferences sharedPreferences = getSharedPreferences(KEY_ST,MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putInt(KEY_CURRENT_ST,currentStyle);
-        editor.apply();
-    }
-    private int getCurrentStyle() {
-        SharedPreferences sharedPreferences = getSharedPreferences(KEY_ST,MODE_PRIVATE);
-        return (sharedPreferences.getInt(KEY_CURRENT_ST,-1));
-
-    }
-
-    private int getRaalStyle(int curentTheme) {
-        switch (curentTheme) {
-            case light:
-                return R.style.light1;
-
-            case dark:
-                return R.style.dark1;
-            default: return 0;
-
-        }
     }
 
 
